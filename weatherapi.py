ce3c60eb6dbeace38f99150a2acefba6c679d1dd
courtneytnewman <70600843+courtneytnewman@users.noreply.github.com>
2020-12-03 20:29:08 -0500

    Update weatherapi.py

--- weatherapi.py
+++ weatherapi.py
@@ -9,7 +9,7 @@ import sqlite3
 
 def get_temp_and_day():
     path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+ "weather.db")
+    conn = sqlite3.connect(path+'/'+ "finaldatabase.db")
     cur = conn.cursor()
     
     d = datetime.date(2018, 11, 30)
@@ -26,22 +26,42 @@ def get_temp_and_day():
 
         r = response["data"]["weather"][0]["avgtempF"]
         temps.append(r)
-        combined  = list(zip(dates, temps))  
+        combined  = list(zip(dates, temps))
+        return combined  
 
-    cur.execute("CREATE TABLE Weather (Date TEXT PRIMARY KEY, Temperature INTEGER)")
+def create_database(cur, conn):
+    data = get_temp_and_day()
     count = 0
-    for combo in range(len(combined)):
+    cur.execute("CREATE TABLE Weather (Date TEXT PRIMARY KEY, Temperature INTEGER)")
+    for tup in range(len(data)):
         if count < 24:
             break
         if cur.execute("SELECT") == None:
-            day = combined[combo][0]
-            temperature = combined[combo][1]
-            cur.execute("INSERT INTO Weather (Date, Temperature) VALUES (?, ?)", day, temperature,)
+            day = data[tup][0]
+            temperature = data[tup][1]
+            cur.execute("INSERT INTO Weather (Date, Temperature) VALUES (?, ?)", (day, temperature,))
             count += 1
     conn.commit()
+         
+    # for i in range(len(category_list)):
+    #     cur.execute("INSERT INTO Weather (date,temp) VALUES (?,?)",(i,category_list[i]))
+    # conn.commit()
+#     #create db file and cur and con -- modify name of file from assignments 
+#     #DONT DO DROP TABLE 
+#     #date PRIMARY KEY -- across ALL DATABASE (makes joins easier)
+#     cur.execute("CREATE TABLE ")
+#     temps = get_temp_and_day() 
+#     count = 0
+#     for x in temps:
+#         #check count var 
+#         if count < 24:
+#             break
+#         #select statement 
+#         if cur.execute("SELECT ") == None:
+#             cur.execute("INSERT date, temp = ? ?",
+#             count += 1
 
-
-
+#     con.commit()
 
 
 

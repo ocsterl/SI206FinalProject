62db3083cb3f6f0dacc938f441b694e358fcbe1f
jacktucker8 <70600861+jacktucker8@users.noreply.github.com>
2020-12-06 14:49:56 -0500

    Update stockAPI.py

--- stockAPI.py
+++ stockAPI.py
@@ -18,11 +18,26 @@ start = datetime(2018, 12, 15)
 end = datetime(2019, 3, 24)
 
 #Connecting to Database
-path = os.path.dirname(os.path.abspath(__file__))
-conn = sqlite3.connect(path + '/' + "finalprojectdatabase.db")
-cur = conn.cursor()
+def SetUpDatabase():
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + '/' + "finaldatabase.db")
+    cur = conn.cursor()
 
 
 #DataFrame date/price
 s = getHistoricalPrices('MTN', start, end)
 price = s.drop(columns = ['volume'])
+
+#List of Prices
+prices = s['close'].to_list()
+
+
+
+index = s.index
+a_list = list(index)
+
+#List of Dates
+dates = []
+for i in range(0, len(a_list)):
+    date_time = a_list[i].strftime("%Y-%m-%d")
+    dates.append(date_time)

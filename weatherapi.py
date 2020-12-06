62573aec4342eff41c424d36e78f0f6ec7941be7
courtneytnewman <70600843+courtneytnewman@users.noreply.github.com>
2020-12-06 15:43:48 -0500

    Update weatherapi.py

--- weatherapi.py
+++ weatherapi.py
@@ -18,6 +18,8 @@ def get_temp_and_day():
     date = str(d)
     dates = []
     temps = []
+    sun = []
+
     for i in range(0, 151):
         d = d + datetime.timedelta(days = 1)
         url = "http://api.worldweatheronline.com/premium/v1/past-weather.ashx?key=5a9833b5e89c46039c8184642200212&q=Vail,co&date=" + str(d) + "&tp=3&format=json"
@@ -28,7 +30,10 @@ def get_temp_and_day():
 
         r = response["data"]["weather"][0]["avgtempF"]
         temps.append(r)
-        combined  = list(zip(dates, temps))
+
+        s = response["data"]["weather"][0]["sunHour"]
+        sun.append(s)
+        combined  = list(zip(dates, temps, sun))
     return combined  
 
 def create_table(cur, conn):
@@ -36,15 +41,20 @@ def create_table(cur, conn):
     data = get_temp_and_day()
     count = 0
     cur.execute("DROP TABLE IF EXISTS Weather")
-    cur.execute("CREATE TABLE Weather (Date TEXT PRIMARY KEY, Temperature INTEGER)")
+    cur.execute("CREATE TABLE Weather (Date TEXT PRIMARY KEY, Temperature INTEGER, HoursOfSun INTEGER)")
     for tup in range(len(data)):
         if count > 24:
             break
         day = data[tup][0]
         temperature = data[tup][1]
-        cur.execute("INSERT INTO Weather (Date, Temperature) VALUES (?, ?)", (day, temperature,))
+        hours = data[tup][2]
+        cur.execute("INSERT INTO Weather (Date, Temperature, HoursOfSun) VALUES (?, ?, ?)", (day, temperature,hours,))
         count += 1
     conn.commit()
+
+# def jointables(cur, conn):
+#     cur.execute("SELECT Weather.Date, Weather.Temperature FROM Weather JOIN ConditioninVail WHERE Weather.Date = ConditioninVail.Date, PercipitationinVail.Date")
+#     conn.commit()
 
 def main():
     combo = get_temp_and_day()

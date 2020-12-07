7cec8983e3f2d0f4db7c99925d11394baef5e2d9
courtneytnewman <70600843+courtneytnewman@users.noreply.github.com>
2020-12-06 21:11:09 -0500

    Update weatherapi.py

--- weatherapi.py
+++ weatherapi.py
@@ -40,17 +40,42 @@ def create_table(cur, conn):
 
     data = get_temp_and_day()
     count = 0
+    temp = []
+    days = []
+    total = 0
+    average = 0
     cur.execute("DROP TABLE IF EXISTS Weather")
     cur.execute("CREATE TABLE Weather (Date TEXT PRIMARY KEY, Temperature INTEGER, HoursOfSun INTEGER)")
     for tup in range(len(data)):
         if count > 24:
             break
         day = data[tup][0]
+        days.append(day)
         temperature = data[tup][1]
+        temp.append(temperature)
         hours = data[tup][2]
         cur.execute("INSERT INTO Weather (Date, Temperature, HoursOfSun) VALUES (?, ?, ?)", (day, temperature,hours,))
         count += 1
     conn.commit()
+
+    cur.execute("DROP TABLE IF EXISTS Averages")
+    cur.execute("CREATE TABLE Averages (Days TEXT PRIMARY KEY, AverageTemp INTEGER)")
+    for t in temp:
+        total = total + int(t)
+    average = total / len(temp)
+
+    dayrange = days[0] + " to " + days[-1]
+
+    print(dayrange)
+
+    cur.execute("INSERT INTO Averages (Days, AverageTemp) VALUES (?, ?)", (dayrange, average,))
+    conn.commit()
+
+        
+        
+
+
+    
 
 # def jointables(cur, conn):
 #     cur.execute("SELECT Weather.Date, Weather.Temperature FROM Weather JOIN ConditioninVail WHERE Weather.Date = ConditioninVail.Date, PercipitationinVail.Date")
@@ -66,4 +91,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
